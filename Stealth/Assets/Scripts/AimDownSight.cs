48fbf6d7fe70b3289685ad617882b47ccaa5fa50
Rowan HUNTER <HUN0006@tc.vic.edu.au>
2018-02-21 20:46:57 +1100

    First commit

---
+++ Stealth/Assets/Scripts/AimDownSight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimDownSight : MonoBehaviour {
+    public Vector3 aimDownSight;
+    public Vector3 hipFire;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            transform.localPosition = aimDownSight;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            transform.localPosition = hipFire;
+        }
+	}
+}
---
+++ Stealth/Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float speed = 20;
+    public Animator anim;
+    public Rigidbody Object;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+
+            anim.Play("Slider Test Animation");
+
+            anim.SetTrigger("Active");
+        }
+    }
+        void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+        
+
+
+
+
+
+    
+}
+
